93a8e7d49fae90a0744d77a81f8f95c31d298cc0
John Doe Antler <16289028+JohnDoeAntler@users.noreply.github.com>
2019-08-09 03:52:44 +0800

    angular js + modified search strategy from equal to contains

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
-let form_submit = ()=>{
-    console.log("test");
-    let plate = document.getElementById("plate").value.replace(/\s/g, '').toUpperCase();
-    console.log(plate);
-    document.getElementById("plate").value = "";    
-    for (let i =0;i<json.length;i++){
-        if (json[i]["plate"] === plate){
-            cmb_true(json[i]);
-            return false;
+let app = angular.module("app", []);
+
+app.controller("controller", ($scope) => {
+    $scope.search = () => {
+        if (!($scope.isRemind = !!!$scope.plate)){
+            let obj = getPlatesByText($scope.plate.toUpperCase());
+            if (!($scope.isRemind = !!!obj)){
+                $scope.title = `Plate: ${obj.plate}`;
+                $scope.desc = `Description: ${obj.description}`;
+            }
         }
     }
-    if (plate.substring(0, 2) === "AM"){
-        cmb_true({
-            "plate" : plate,
-            "description" : "AM車（政府車）"
-        });
-        return false;
-    }
-    cmb_false(plate);
+})
 
-    return false;
+let getPlatesByText = (plate) => {
+    return isAM(plate) || json.filter(x => x.plate.toUpperCase().includes(plate))[0];
 }
 
-let cmb_true = info =>{
-    let cmb = document.getElementById("cmb");
-    cmb.style.visibility = "visible";
-    cmb.classList.add("alert-danger");
-    cmb.classList.remove("alert-success");
-    cmb.innerHTML = "<strong>CMB plate match!</strong><br>Plate: " + info["plate"] + "<br>Description: " + info["description"] + "<br>";
-}
-
-let cmb_false = plate => {
-    let cmb = document.getElementById("cmb");
-    cmb.style.visibility = "visible";
-    cmb.classList.add("alert-success");
-    cmb.classList.remove("alert-danger");
-    cmb.innerHTML = "<strong>CMB plate not match!</strong><br>Plate: " + plate + "<br>Stay alert and be water my friend!<br>";
+let isAM = (plate) => {
+    return plate.substring(0, 2) === "AM" ? {"plate" : plate, "description" : "AM車（政府車）"} : undefined;
 }
