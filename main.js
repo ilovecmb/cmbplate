313828eb71ec28f5043b012dece356c4afe0542c
chungonion <chungonion20@yahoo.com.hk>
2019-08-09 10:17:49 +0800

    Logic refractoring + cleanup

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let form_submit = ()=>{
-    console.log("test");
     let plate = document.getElementById("plate").value.replace(/\s/g, '').toUpperCase();
     console.log(plate);
-    document.getElementById("plate").value = "";    
+    document.getElementById("plate").value = "";
+
+    //data search for matching exact plate number in the data file
     for (let i =0;i<json.length;i++){
         if (json[i]["plate"] === plate){
             cmb_true(json[i]);
@@ -10,29 +11,10 @@ let form_submit = ()=>{
         }
     }
 
-    switch (plate.substring(0, 2)){
-    case "AM":
-        cmb_true({
-            "plate" : plate,
-            "description" : "AM車（政府車）"
-        });
-        return false;
-    case "ZG":
-        cmb_true({
-            "plate" : plate,
-            "description" : "ZG車（解放軍駐港部隊）"
-        });
-        return false;
-    case "FU":
-    case "FV":
-        cmb_true({
-            "plate" : plate,
-            "description" : "FU/FV車（<strong>可能</strong>是中國左軚車）"
-        });
-        return false;
+    //generic search for certain pattern of plate numbers
+    if (!generic_search(plate)){
+        cmb_false(plate);
     }
-
-    cmb_false(plate);
 
     return false;
 }
@@ -51,4 +33,32 @@ let cmb_false = plate => {
     cmb.classList.add("alert-success");
     cmb.classList.remove("alert-danger");
     cmb.innerHTML = "<strong>CMB plate not match!</strong><br>Plate: " + plate + "<br>Stay alert and be water my friend!<br>";
+}
+
+let generic_search = plate =>{
+    //generic_search return statement explanation
+    //true => match generic criteria ("e.g. certain plate pattern").
+    //false => reverse of the above statement.
+    switch (plate.substring(0, 2)){
+        case "AM":
+            cmb_true({
+                "plate" : plate,
+                "description" : "AM車（政府車）"
+            });
+            return true;
+        case "ZG":
+            cmb_true({
+                "plate" : plate,
+                "description" : "ZG車（解放軍駐港部隊）"
+            });
+            return true;
+        case "FU":
+        case "FV":
+            cmb_true({
+                "plate" : plate,
+                "description" : "FU/FV車（<strong>可能</strong>是中國左軚車）"
+            });
+            return true;
+    }
+    return false;
 }
